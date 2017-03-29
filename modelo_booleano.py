63788d0ef1a8a07ef362a94c56856e4c6bfa51e0
fabiano <fabiano.paula.martins@gmail.com>
2017-03-29 20:33:00 -0300

    Creating folder models to store the classes. Removing old or renamed files

--- modelo_booleano.py
+++ modelo_booleano.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python
 # encoding: UTF-8
 
 import numpy as np
+from numpy import log2
 import sys
-
-
-M= [u"O peã e o caval são pec de xadrez. O caval é o melhor do jog.",
-    u"A jog envolv a torr, o peã e o rei.",
-    u"O peã lac o boi",
-    u"Caval de rodei!",
-    u"Polic o jog no xadrez."]; #conjunto de documentos
-
-stopwords=[u"a",u"o",u"e",u"é",u"de",u"do",u"no",u"são"]; #lista de stopwords
-q=u"xadrez peã caval torr"; #consulta
-separadores=[u" ",u",",u".",u"!",u"?"]; #separadores para tokenizacao
 
 def tokenizador(palavra,separadores):
     lista_tokens = []
@@ -44,32 +33,7 @@ def normalizar(lista_tokens,stopwords):
             lista_tokens_sem_stopwords.append(w.lower())
     return lista_tokens_sem_stopwords
 
-index = [normalizar(tokenizador(doc,separadores),stopwords) for doc in M]
 
-# Calculando o número de tokens 
-# essa lista funciona como um dicionário 
-# de posição nas colunas da matriz de incidência
-
-tokens = []
-
-for doc_index in index:
-	for word in doc_index:
-		if not( word in tokens):
-			tokens.append(word)
-
-#for w in tokens:
-#	print w
-
-n_docs = len(index)
-n_tokens = len(tokens)
-
-
-# Criando a matriz de incidência
-matriz_incidencia = np.zeros(n_docs*n_tokens).reshape(n_tokens,n_docs)
-
-for i in range(len(index)):
-	for word in index[i]:
-		matriz_incidencia[tokens.index(word),i] += 1
 
 def list_and(lista1,lista2):
 	resposta = []
@@ -95,36 +59,111 @@ def list_not(lista):
 		resposta.append(1**lista[i])
 	return resposta
 
+
+def preprocess_database(M,stopwords,separadores):
+    index = [normalizar(tokenizador(doc,separadores),stopwords) for doc in M]
+
+    # Calculando o número de tokens
+    # essa lista funciona como um dicionário
+    # de posição nas colunas da matriz de incidência
+    tokens = []
+    for doc_index in index:
+	for word in doc_index:
+            if not( word in tokens):
+		tokens.append(word)
+
+    n_docs = len(index)
+    n_tokens = len(tokens)
+
+    # Criando a matriz de incidência
+    matriz_incidencia = np.zeros(n_docs*n_tokens).reshape(n_tokens,n_docs)
+    matriz_pesos = np.zeros(n_docs*n_tokens).reshape(n_tokens,n_docs)
+    n = np.zeros(n_tokens)
+
+    for i in range(len(index)):
+	for word in index[i]:
+		matriz_incidencia[tokens.index(word),i] += 1
+
+    # Calculando o vetor n
+    # Esse vetor contém quantos documentos o token apareceu
+    for i in range(matriz_incidencia.shape[0]):
+        for num in matriz_incidencia[i]:
+            if( num > 0):
+                n[i] += 1
+    # Calculando os pesos das consultas usando o esquema três
+    for i in range(matriz_incidencia.shape[0]):
+        for j in range(matriz_incidencia.shape[1]):
+            if not( matriz_incidencia[i,j] == 0):
+                matriz_pesos[i,j] = (1 + log2(matriz_incidencia[i,j]))* \
+                                log2(n_docs/n[i])
+
+    for i in range(len(tokens)):
+        sys.stdout.write(str(matriz_incidencia[i]))
+        print tokens[i]
+
+    return(matriz_incidencia,matriz_pesos,tokens,n)
+
+
+# preprocessamento da query
+# mi = matriz de incidência
+# mp = matriz de pesos
+
+def preprocess_query(n,mi,tokens_d,query,stopwords,separadores):
+    n_docs_d = mi.shape[1]
+    index = [normalizar(tokenizador(query,separadores),stopwords)]
+
+    # Calculando o número de tokens
+    # essa lista funciona como um dicionário
+    # de posição nas colunas da matriz de incidência
+    tokens = []
+    for doc_index in index:
+	for word in doc_index:
+            if not( word in tokens):
+		tokens.append(word)
+
+    n_docs = len(index)
+    n_tokens = len(tokens)
+
+    # Criando a matriz de incidência
+    matriz_incidencia = np.zeros(n_docs*n_tokens).reshape(n_tokens,n_docs)
+    matriz_pesos = np.zeros(n_docs*n_tokens).reshape(n_tokens,n_docs)
+
+    for i in range(len(index)):
+    	for word in index[i]:
+  	    matriz_incidencia[tokens.index(word),i] += 1
+
+    # Calculando os pesos das consultas usando o esquema três
+    for i in range(matriz_incidencia.shape[0]):
+        for j in range(matriz_incidencia.shape[1]):
+            if not( matriz_incidencia[i,j] == 0):
+                matriz_pesos[i,j] = (1 + log2(matriz_incidencia[i,j]))* \
+                                log2(n_docs_d/n[tokens_d.index(tokens[i])])
+
+    return(matriz_incidencia,matriz_pesos,tokens)
+
+
+# função que processa a query
 # mi = matriz de incidencia
-# t = lista de tokens 
-
-for i in range(len(tokens)):
-    sys.stdout.write(str(matriz_incidencia[i]))
-    print tokens[i]
-
+# t = lista de tokens
 def process(mi,t,query):
-	lista_resposta = []
-	listand = query.split(' ')
+    lista_resposta = []
+    listand = query.split(' ')
 
-#	print query
 
-	if(len(listand) > 1):
-		lista_resposta = list_and(mi[t.index(listand[0])],mi[t.index(listand[1])])
-		for i in range(2,len(listand)):
-			print lista_resposta
-			lista_resposta = list_and(lista_resposta,mi[t.index(listand[i])])
-#		print lista_resposta
-	else:
-		lista_resposta = mi[t.index(listand[0])]
+
+
+    if(len(listand) > 1):
+	lista_resposta = list_and(mi[t.index(listand[0])],mi[t.index(listand[1])])
+	for i in range(2,len(listand)):
+		print lista_resposta
+		lista_resposta = list_and(lista_resposta,mi[t.index(listand[i])])
+    else:
+	lista_resposta = mi[t.index(listand[0])]
 
 	resposta = []
 	for i in range(len(lista_resposta)):
 		if( lista_resposta[i] == 1):
 			resposta.append(i+1)
 	return resposta
-
-print process(matriz_incidencia,tokens,q)
-print process(matriz_incidencia,tokens,u"xadrez peã")
-
 
 
---
+++ models/boolean_model.py
@@ -0,0 +1,81 @@
+# encoding: UTF-8
+
+from numpy import zeros
+
+from base_model import BaseModel
+
+class BooleanModel(BaseModel):
+
+    def list_and(self,list1,list2):
+        response = []
+        for i in range(len(list1)):
+            if not( list1[i]*list2[i] == 0):
+                response.append(1)
+            else:
+                response.append(0)
+        return response
+
+    def list_or(self,list1,list2):
+        response = []
+        for i in range(len(list1)):
+            if not( list1[i] + list2[i] == 0):
+                response.append(1)
+            else:
+                response.append(0)
+        return response
+
+    def list_not(self,list1):
+        response = []
+        for i in range(len(list1)):
+            if( list1[i] == 0)
+                response.append(1)
+            else:
+                response.append(0)
+        return response
+
+    #-------------------------------------------------------------------------
+    #             PROCESS AND PREPROCESS FUNCTIONS
+    #-------------------------------------------------------------------------
+
+    # preprocessing of the database
+    def preprocess_database(self,database):
+        self.index = []
+        for doc in database:
+            token_list = super(BooleanModel,self).tokenizador(doc)
+            self.index.append(super(BooleanModel,self).normalizar(token_list))
+
+        self.tokens = []
+        for doc_index in self.index:
+            for word in doc_index:
+                if not(word in tokens):
+                    self.tokens.append(word)
+
+        self.n_docs   = len(self.index)
+        self.n_tokens = len(self.tokens)
+
+        # Incidence Matrix
+        self.im = zeros(self.n_docs*self.n_tokens).reshape(self.n_tokens,self.n_docs)
+
+        for i in range(len(self.index)):
+            for word in index[i]:
+                im[self.tokens.index(word),i] += 1
+
+
+    # function that process a query
+    def process(self,query):
+        listand = query.split(' ')
+        response_list = []
+
+        if(len(listand) > 1):
+            response_list = list_and(self.im[self.tokens.index(listand[0])],self.im[self.tokens.index(listand[1])])
+            for i in range(2,len(listand)):
+                response_list = list_and(response_list,self.im[self.tokens.index(listand[i])])
+        else:
+            response_list = self.im[self.tokens.index(listand[0])]
+
+        result = []
+        for i in range(len(response_list)):
+            if(response_list[i] == 1):
+                    result.append(i+1)
+        return result 
+
